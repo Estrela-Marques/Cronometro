16b7ab0fc0c9d7edaf34137d2f673853da38d31c
Estrela-Marques <estelapfarma@gmail.com>
2024-01-31 09:23:14 -0300

    Cronometro

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿
+namespace Stopwatch
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Menu();
+    }
+
+    static void Menu()
+    {
+      Console.WriteLine("S = Segundo => 10s = 10 segundos");
+      Console.WriteLine("M = Minuto => 1m = 1 minuto");
+      Console.WriteLine("0s = Sair");
+      Console.WriteLine("Quanto tempo deseja contar?");
+
+      string data = Console.ReadLine().ToLower(); //ReadLine -> lê uma linha. ToLower -> Converte para minusculo.  
+      char type = char.Parse(data.Substring(data.Length - 1, 1)); // pega o ultimo caractere que o usuario digitou
+      int time = int.Parse(data.Substring(0, data.Length - 1));
+      int multiplier = 1;
+
+      if (type == 'm')
+        multiplier = 60;
+
+      if (time == 0)
+        System.Environment.Exit(0);
+
+      PreStart(time * multiplier);
+
+    }
+
+    static void PreStart(int time)
+    {
+      Console.Clear();
+      Console.WriteLine("Preparar...");
+      Thread.Sleep(1000);
+      Console.WriteLine("Apontar...");
+      Thread.Sleep(1000);
+      Console.WriteLine("Já...");
+      Thread.Sleep(2500);
+
+      Start(time);
+    }
+    static void Start(int time)
+    {
+
+      int currentTime = 0;
+
+      while (currentTime != time)
+      {
+        Console.Clear();
+        currentTime++;
+        Console.WriteLine(currentTime);
+        Thread.Sleep(1000);
+      }
+
+      Console.Clear();
+      Console.WriteLine("Cronometro finalizado!");
+      Thread.Sleep(2500);
+      Menu();
+    }
+  }
+}
+
